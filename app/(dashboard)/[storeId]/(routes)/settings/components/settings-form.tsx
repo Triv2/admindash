40448f218bd363bf3200f7be4f011f93e581038e
Trivial-lore <triviallore@gmail.com>
2023-08-17 14:37:33 -0500

    settings name change setup

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -10,7 +10,8 @@ import { Store } from "@prisma/client";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
-import { Form, FormField } from "@/components/ui/form";
+import { Form, FormField, FormItem, FormLabel, FormControl, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 
 interface SettingsFormProps {
@@ -43,12 +44,13 @@ export const SettingsForm: React.FC<SettingsFormProps>= ({
     <div className="flex items-center justify-between">
       <Heading
       title="Settings"
-      description="Manage store preferences"
+      description="Manage Store Preferences"
       />
       <Button
+      disabled={loading}
       variant="destructive"
       size="icon"
-      onClick={()=>{}}
+      onClick={()=>{setOpen(true)}}
       >
         <Trash className="h-4 w-4"/>
       </Button>
@@ -57,8 +59,25 @@ export const SettingsForm: React.FC<SettingsFormProps>= ({
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8  w-full">
        <div className="grid grid-cols-3 gap-8">
-
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>
+                Name
+              </FormLabel>
+              <FormControl>
+               <Input disabled={loading} placeholder="Store name" {...field}/>
+              </FormControl>
+              <FormMessage/>
+            </FormItem>
+            )}
+        />
        </div>
+       <Button disabled={loading} className="ml-auto" type="submit">
+        Save Changes
+       </Button>
       </form>
     </Form>
   </>
