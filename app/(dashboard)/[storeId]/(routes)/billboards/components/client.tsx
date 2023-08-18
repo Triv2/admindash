6ee659f901220968fbd9c202c1c2eddc1b22b6c8
Trivial-lore <triviallore@gmail.com>
2023-08-18 15:37:36 -0500

    basic data table setup

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -3,12 +3,14 @@
 import { Button } from "@/components/ui/button"
 import { Heading } from "@/components/ui/heading"
 import { Separator } from "@/components/ui/separator"
-import { Billboard } from "@prisma/client"
+
 import { Plus } from "lucide-react"
 import { useParams, useRouter } from "next/navigation"
+import { BillboardColumn, columns } from "./columns"
+import { DataTable } from "@/components/ui/data-table"
 
 interface BillboardClientProps {
-  data:Billboard[]
+  data:BillboardColumn[]
 }
 
 export const BillboardClient:React.FC<BillboardClientProps> = ({
@@ -16,7 +18,7 @@ export const BillboardClient:React.FC<BillboardClientProps> = ({
 }) => {
   const router=useRouter();
   const params=useParams();
-  
+
   return (
     <>
     <div className="flex items-center justify-between">
@@ -30,6 +32,10 @@ export const BillboardClient:React.FC<BillboardClientProps> = ({
       </Button>
     </div>
     <Separator/>
+    <DataTable
+      columns={columns}
+      data={data}
+    />
     </>
   )
 }
