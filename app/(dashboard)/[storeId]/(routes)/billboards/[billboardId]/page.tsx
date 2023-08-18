51a423f0ef0882391b689e133b5908d86a7b7f40
Trivial-lore <triviallore@gmail.com>
2023-08-17 23:02:48 -0500

    setting up billboard entities

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -0,0 +1,22 @@
+import prismadb from "@/lib/prismadb";
+
+const BillboardPage = async ({
+  params
+} : {
+  params: {billboardId: string}
+}) => {
+
+  const billboard = await prismadb.billboard.findUnique({
+    where:{
+      id: params.billboardId
+    }
+  });
+    
+  return (
+    <div>
+        This is a form for billboards.
+    </div>
+  )
+}
+
+export default BillboardPage;
