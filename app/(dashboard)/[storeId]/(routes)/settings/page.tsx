0f7e7a96c0e0a89980b59997217f18646df6ba8f
Trivial-lore <triviallore@gmail.com>
2023-09-28 17:30:42 -0500

    updated to v13.5.5 next

--- app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -1,6 +1,7 @@
 import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+
 import SettingsForm from "./components/settings-form";
 
 interface SettingsPageProps {
